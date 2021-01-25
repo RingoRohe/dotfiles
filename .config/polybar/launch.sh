1e040e5145234e96f34524460ac72912dc3fdb91
Ringo Rohe <rrohe@matero.de>
2021-01-25 21:41:32 +0100

    new polybar style

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar topbar 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar_top.log & disown
+polybar bottom 2>&1 | tee -a /tmp/polybar_bottom.log & disown
 
 echo "Bars launched..."
