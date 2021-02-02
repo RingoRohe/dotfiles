07d340ab1271d073ba89400e6defc09d84a8183f
Ringo Rohe <rrohe@matero.de>
2021-02-02 20:57:05 +0100

    made rofi shutdown script variable

---
+++ .config/rofi/scripts/shutdown_dialog.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# a simple dmenu session script 
+#
+###
+
+hostname=$(echo uname -n)
+
+DMENU='rofi -dmenu'
+choice=$(echo -e "shutdown\nreboot\nsuspend\nlogout\nhibernate\nlock" | $DMENU)
+
+if [ "$hostname" = "ManjaroVM" ]; then
+    case "$choice" in
+        shutdown) i3exit shutdown & ;;
+        reboot) i3exit reboot now & ;;
+        suspend) i3exit suspend & ;;
+        logout) i3-msg exit & ;;
+        hibernate) i3exit hibernate & ;;
+        lock) betterlockscreen -l blur & ;;
+    esac
+fi
+
+if [ "$hostname" = "ThinkPad-T570" ]; then
+    case "$choice" in
+        shutdown) cinnamon-session-quit --power-off & ;;
+        reboot) cinnamon-session-quit --reboot & ;;
+        suspend) systemctl suspend & ;;
+        logout) cinnamon-session-quit --logout & ;;
+        hibernate) systemctl hibernate & ;;
+        lock) cinnamon-screensaver-command --lock & ;;
+    esac
+fi
