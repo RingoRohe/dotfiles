760ed89dfcc1c5bf6db076715bc4f503ab9c45f8
Ringo Rohe <hallo@ringorohe.de>
2024-10-14 13:41:50 +0200

    nvim config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- nmap <C-PageUp> ddkP
+-- nmap <C-PageDown> ddp
+vim.keymap.set("n", "<C-PageUp>", "ddkP")
+vim.keymap.set("n", "<C-PageDown>", "ddp")
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,21 @@
 return {
-  --  { "shaunsingh/nord.nvim" },
+  { "whatyouhide/vim-gotham" },
+  { "cocopon/iceberg.vim" },
+  { "tomasr/molokai" },
+  { "dikiaap/minimalist" },
+  { "nanotech/jellybeans.vim" },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "darker",
+    },
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "default",
+      colorscheme = "jellybeans",
     },
   },
 }
